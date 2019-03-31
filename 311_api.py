e0fce1e7cffef15462e190490f4dce89b923cdd2
Mcgrupp34 <zackwalker8@gmail.com>
2019-03-31 13:39:02 -0400

    Update 311_api.py
    update to 311 api call

--- 311_api.py
+++ 311_api.py
@@ -1,12 +1,8 @@
-# make sure to install these packages before running:
-# pip install pandas
-# pip install sodapy
+
 
 import pandas as pd
 from sodapy import Socrata
 
-# Unauthenticated client only works with public data sets. Note 'None'
-# in place of application token, and no username or password:
 client = Socrata("data.cityofnewyork.us", None)
 
 # Example authenticated client (needed for non-public datasets):
